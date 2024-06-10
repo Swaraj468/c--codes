725c08dd73f5dab223d9f022e06db649f3296296
Swaraj <swarajbandhi0000@gmail.com>
2024-06-10 22:25:39 +0530

    Done some contine, prime number print and some crazy homework output questions

---
+++ continue.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(void){
+
+for(int i=0; i<5; i++){
+    cout<<" Hi "<<endl;
+    cout<<" Hey "<<endl;
+    continue;
+    cout<<"Reply toh karde  "<<endl;
+}
+
+
+
+}
