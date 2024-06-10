5ddd0ccd86c821aeeb5b0b7cf283fd436156f69c
Swaraj <swarajbandhi0000@gmail.com>
2024-06-10 21:49:27 +0530

    till here we have learnt bitwise operator and some for loop concepts and done a example of for loop and fibonacci series

---
+++ forloop.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int n;
+    cout<<" enter the value of n "<<endl;
+   // cin>>n;
+    cout<<"printing cout from 1 to n" << endl;
+    
+    // for(int i = 1; i<=n; i++){
+    // cout<<i<<endl;
+    // }
+    for(int a=0, b=1; a>=0 && b>=1; a--, b-- ){
+        cout<<a<<" "<< b << endl;
+    }
+}
