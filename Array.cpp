f222c725a1f11934221a59edd3dfc0c0d62c3c5b
Swaraj <swarajbandhi0000@gmail.com>
2024-06-15 23:16:39 +0530

    Learn the concept of two pointers and printing 0's and 1's

--- Array.cpp
+++ Array.cpp
@@ -1,22 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-int findMax(int arr[], int size);
 
-bool findTarget(int arr[], int size, int target);
 
-void solve1(int arr[], int n);
 
-void print(int arr[5], int size);
+// void printZerosAndOnes(int arr[], int n);
+// int findMax(int arr[], int size);
 
-int main(void){
+// bool findTarget(int arr[], int size, int target);
 
-int arr[]={1,2,4,5,6};
-int size =5;
-for(int i=0; i<size; i++){
-    cout<<"Enter the input value for index: "<< i <<endl;
-    cin>>arr[i];
-}
-cout<<" max number is: "<< findMax(arr, size) <<endl;
+// void solve1(int arr[], int n);
+
+// void print(int arr[5], int size);
+void TwoPointer(int arr[], int size);
+
+int main(){
+
+int arr[] = {10, 20, 30, 40, 50, 60, 70};
+int size = 7;
+TwoPointer(arr, size);
+
+// int arr[]={1,2,4,5,6};
+// int size =5;
+// for(int i=0; i<size; i++){
+//     cout<<"Enter the input value for index: "<< i <<endl;
+//     cin>>arr[i];
+// }
+
+// printZerosAndOnes(arr, size);
+
+
+// cout<<" max number is: "<< findMax(arr, size) <<endl;
 
 // cout<<"Enter your target: "<<endl;
 // cin>>target;
@@ -46,31 +59,70 @@ cout<<" max number is: "<< findMax(arr, size) <<endl;
 
 //     return 0;
 }
-int findMax(int arr[], int size){
-    int maxans = INT_MIN;
-    for(int i=0; i<size; i++){
-        maxans= max(maxans, arr[i]);
+void TwoPointer(int arr[], int size){
+    int i=0;
+    int j=size-1;
+    while(i<=j){
+        if(i==j){
+            // print only 1 times
+            cout<< arr[i] <<" ";
+            i++;
+            // or we can use break instead of i+_
 
-    }
-    return maxans;
-}
-bool findTarget(int arr[], int size, int target){
-    for(int i=0; i<size; i++){
-       
-        if(arr[i] == target){
-            return true;
+        }else{
+
+        
+        cout<<arr[i]<<" ";
+        i++;
+        cout<<arr[j]<<" ";
+        j--;
         }
     }
-    return false;
 }
-void solve1(int arr[], int n){
-    for(int i=0; i<n; i++){
-        arr[i]=arr[i] * 10;
-    }
-}
-void print(int arr[5], int size){
-    for(int i=0; i<size; i++){
-        cout<<arr[i]<<" ";
-    }
+// void printZerosAndOnes(int arr[], int size){
+//     int zeroCount = 0;
+//     int oneCount = 0;
 
-}
+//     //traverse Array
+//     for(int i=0; i<size; i++){
+        
+//         if(arr[i]==0){
+//             zeroCount++;
+
+//         }
+//         if(arr[i]==1){
+//             oneCount++;
+//         }
+//     }
+//     cout<<"Total zeros: "<<zeroCount <<endl;
+//     cout<<"Total Ones: "<<oneCount<<endl;
+    
+// }
+// int findMax(int arr[], int size){
+//     int maxans = INT_MIN;
+//     for(int i=0; i<size; i++){
+//         maxans= max(maxans, arr[i]);
+
+//     }
+//     return maxans;
+// }
+// bool findTarget(int arr[], int size, int target){
+//     for(int i=0; i<size; i++){
+       
+//         if(arr[i] == target){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// void solve1(int arr[], int n){
+//     for(int i=0; i<n; i++){
+//         arr[i]=arr[i] * 10;
+//     }
+// }
+// void print(int arr[5], int size){
+//     for(int i=0; i<size; i++){
+//         cout<<arr[i]<<" ";
+//     }
+
+// }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,43 +1,47 @@
-#include<bits/stdc++.h>
-using namespace std;
+void printZerosAndOnes(int arr[], int size){
+//     int zeroCount = 0;
+//     int oneCount = 0;
 
-void solve1(int arr[]; int n);
+//     //traverse Array
+//     for(int i=0; i<size; i++){
+        
+//         if(arr[i]==0){
+//             zeroCount++;
 
-void print(int arr[5], int size);
-
-int main(void){
-
-int arr[]={1,2,4,5,6};
-int size =5;
-solve1(arr, size);
-print(arr,size);
-// int brr[5]={1,2,3,4,5};
-// int crr[4]={10};
-
-// fill(arr, arr+10, 23);
-
-// cout<<brr[0]<<endl;
-// cout<<crr[0]<<endl;
-// cout<<arr[0]<<" "<<arr[1]<<endl;
-// for(int i = 0; i<5; i++){
-//     cout<<"Enter the value for box index: "<< i <<endl;
-//     cin>>arr[i];
+//         }
+//         if(arr[i]==1){
+//             oneCount++;
+//         }
+//     }
+//     cout<<"Total zeros: "<<zeroCount <<endl;
+//     cout<<"Total Ones: "<<oneCount<<endl;
+    
 // }
-// for(int i=0; i<5; i++){
-//     cout<<arr[i]<<" ";
+// int findMax(int arr[], int size){
+//     int maxans = INT_MIN;
+//     for(int i=0; i<size; i++){
+//         maxans= max(maxans, arr[i]);
+
+//     }
+//     return maxans;
 // }
+// bool findTarget(int arr[], int size, int target){
+//     for(int i=0; i<size; i++){
+       
+//         if(arr[i] == target){
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+// void solve1(int arr[], int n){
+//     for(int i=0; i<n; i++){
+//         arr[i]=arr[i] * 10;
+//     }
+// }
+// void print(int arr[5], int size){
+//     for(int i=0; i<size; i++){
+//         cout<<arr[i]<<" ";
+//     }
 
-
-//     return 0;
-}
-void solve1(int arr[], int n){
-    for(int i=0; i<n; i++){
-        arr[i]=arr[i]*10;
-    }
-}
-void print(int arr[5], int size){
-    for(int i=0; i<size; i++){
-        cout<<arr[i]<<" ";
-    }
-
-}
+// }
