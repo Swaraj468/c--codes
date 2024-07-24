319305a837c0ce0cd030f9bdbd8c7b909ca82e2b
Swaraj <swarajbandhi0000@gmail.com>
2024-07-24 16:26:48 +0530

    so tried to modify and add some functions in 2d vector but it didn,t workout so i guess we will try later

--- 2D-vector.cpp
+++ 2D-vector.cpp
@@ -1,13 +1,23 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(void){
-    // 2D array vector
-    vector<vector<int>>arr(5, vector<int>(4, 0));
-    // 2D array created with 5 rows with 4 columns, with 0 initial values;
-    // for rows count
-    int total_rows = arr.size();
-    // ;for columns count
-    int total_columns = arr[0].size();
-    cout<<total_rows<<" "<< total_columns<<" "<<endl;
+    vector<vector<int>>arr(4);
+    arr[0]=vector<int>(4);
+    arr[1]=vector<int>(2);
+    arr[2]=vector<int>(5);
+    arr[3]=vector<int>(3);
+    // row count
+    int total_row = arr.size();
+    // column count
+    int total_column = arr[0].size();
+    cout<<total_row<<" "<<total_column<<" "<<endl;
+    arr.push_back(vector<int>(1, 10));
+    arr.push_back(vector<int>(1, 20));
+    arr.push_back(vector<int>(1, 30));
+    arr.push_back(vector<int>(1, 40));
+    cout<<"Size: "<< arr.size()<<endl;
+    arr.pop_back();
+    cout<<arr.front().size()<<endl;
+    cout<<arr.back().size()<<endl;
     
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,4 @@
-marks.clear();
+arr[0]=vector<int>(4);
+    arr[1]=vector<int>(2);
+    arr[2]=vector<int>(5);
+    arr[3]=vector<int>(3);
