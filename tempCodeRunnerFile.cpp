724c2488a9813315a09e1fba2a93bad75266baaa
Swaraj <swarajbandhi0000@gmail.com>
2024-07-24 15:41:33 +0530

    so done with 1d vector and tried some function like insert, push back, pop back, capacity, erarse erase, swap, iterator, front, back etc.

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,18 +1,1 @@
-#include<bits/stdc++.h>
-using namespace std;
-void populate(int arr[],int n);
-int main(void){
-    int arr[];
-
-    int n;
-    populate(arr, n);
-}
-void populate(int arr[],int n){
-    int j=0;
-    for(int i=0; i < n; i = i + 2){
-        arr[n-j] = i+2;
-        arr[j] = i+1;
-        j++;
-    }
-    cout<<arr[j]<<endl;
-}
+marks.clear();
--- vector1.cpp
+++ vector1.cpp
@@ -1,7 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(void){
-	vector<int>marks(5, -1);
+	// vector<int>marks(5, -1);
 	// cout<< *(marks.begin())<<endl;
 	// return 0;
+
+// 	vector<int> marks;
+// 	marks.push_back(10);
+// 	marks.push_back(100);
+// 	marks.push_back(200);
+// 	marks.push_back(300);
+// 	cout<<"Size: "<<marks.size()<<endl;
+// 	// marks.clear();
+// 	marks.pop_back();
+// 	cout<<"Size: "<<marks.size()<<endl;
+// 	cout<<marks.front()<<endl;
+//     cout<<marks.back()<<endl;
+// 	marks.insert(marks.begin(), 50);
+// 	cout<<marks.size()<<endl;
+// 	cout<<marks[0]<<endl;
+// 	cout<<marks.capacity()<<endl;
+// 	cout<<marks.at(0)<<endl;
+// 	// marks.erase(marks.begin(), marks.end());
+// 	// cout<<marks.size()<<endl;
+// 	vector<int> second = {1, 2, 3, 4};
+// // swwap
+// marks.swap(second);
+// cout<<marks[0]<<" "<<marks[1]<<" "<<marks[2]<<" "<<marks[3]<<endl;
+vector<int> first;
+
+first.push_back(10);
+first.push_back(11);
+first.push_back(12);
+first.push_back(13);
+// traversing the vector using iterator
+// create an iterator
+
+vector<int>::iterator it = first.begin();
+while(it != first.end()){
+	cout<<*it<<" ";
+	it++;
+}
+
 }
